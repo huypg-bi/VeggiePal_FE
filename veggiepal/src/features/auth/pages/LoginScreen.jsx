285efac355409792e9d523621376b70e04b1160d
huypg-bi <phamgiahuy2707@gmail.com>
2026-09-14 20:01:10 +0700

    style: add gradient bg for login & register screens

--- veggiepal/src/features/auth/pages/LoginScreen.jsx
+++ veggiepal/src/features/auth/pages/LoginScreen.jsx
@@ -3,7 +3,7 @@ import bgLogin from "@/assets/img/bg_login_1.png";
 
 export default function LoginScreen() {
   return (
-    <div className="flex min-h-dvh items-center justify-center bg-gradient-to-br from-[#BFFFC7] via-[white] to-[#BFFFC7] px-6 py-10">
+    <div className="flex min-h-dvh items-center justify-center bg-gradient-to-br from-[#18A575] via-[white] to-[#1E5B53] px-6 py-10">
       <div className="flex w-full max-w-[1040px] flex-col items-center lg:flex-row lg:items-stretch lg:justify-center lg:gap-10 lg:rounded-[32px] lg:bg-[#EEFDEA] lg:p-10 lg:shadow-[0_20px_25px_-5px_rgba(0,0,0,0.08),0_8px_10px_-6px_rgba(0,0,0,0.05)]">
         <img
           src={bgLogin}
